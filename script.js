25e2e4a6a273d2cf1aa2706bb0c119a7912fd707
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-04 01:22:21 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function classeAleatoria() {
+  const cartaMisteriosa = document.querySelectorAll('span');
+  const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
+  const grupoTamanho = ['medium', 'big', 'reallybig'];
+  const grupoRotacao = ['rotateleft', 'rotateright'];
+  const grupoInclinacao = ['skewleft', 'skewright'];
+  const classeEstilo = Math.floor(Math.random() * grupoEstilo.length);
+  const classeTamanho = Math.floor(Math.random() * grupoTamanho.length);
+  const classeRotacao = Math.floor(Math.random() * grupoRotacao.length);
+  const classeInclinacao = Math.floor(Math.random() * grupoInclinacao.length);
+
+  for (let i = 0; i < cartaMisteriosa.length; i += 1) {
+    cartaMisteriosa[i].className = `
+    ${grupoEstilo[classeEstilo]} ${grupoTamanho[classeTamanho]} 
+    ${grupoRotacao[classeRotacao]} ${grupoInclinacao[classeInclinacao]}`;
+  }
+}
+
 function gerarCarta() {
   const cartaTexto = document.getElementById('carta-texto');
   const cartaGerada = document.getElementById('carta-gerada');
@@ -10,6 +28,7 @@ function gerarCarta() {
       cartaGerada.appendChild(document.createElement('span')).innerText = frase[i];
     }
   }
+  classeAleatoria();
 }
 
 document.getElementById('criar-carta').addEventListener('click', gerarCarta);
