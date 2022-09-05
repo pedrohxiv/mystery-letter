d8f3b551701768f37fd6527c4c290d6a78e10f40
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-05 15:33:08 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -38,14 +38,18 @@ function palavraEspecifica(event) {
 function gerarCarta() {
   const cartaTexto = document.getElementById('carta-texto');
   const cartaGerada = document.getElementById('carta-gerada');
+  const cartaContador = document.getElementById('carta-contador');
   const frase = cartaTexto.value.split(' ');
+  const numPavavrasGeradas = frase.length;
   if (cartaTexto.value.trim().length <= 0) {
     cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+    cartaContador.innerText = '';
   } else {
     cartaGerada.innerText = '';
     for (let i = 0; i < frase.length; i += 1) {
       cartaGerada.appendChild(document.createElement('span')).innerText = frase[i];
     }
+    cartaContador.innerText = numPavavrasGeradas;
   }
   classeAleatoria();
 }
